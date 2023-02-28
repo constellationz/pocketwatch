975e756d8de6904b5e70dd16185e54f3e92e2ceb
constellationz <t.angley@protonmail.com>
2023-02-28 05:27:09 -0500

    update genssl

--- scripts/genssl.sh
+++ scripts/genssl.sh
@@ -1,5 +1,8 @@
+#!/usr/bin/bash
 # Generate SSL certificate to sslcert/server.key and sslcert/server.cert
 
 SSLDIR="$(dirname 0)/../sslcert/"
+
+mkdir -p "$SSLDIR"
 
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $SSLDIR/server.key -out $SSLDIR/server.crt
