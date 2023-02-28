6aa53f93aa16f5dbd35c0426de38d641ad51e594
constellationz <t.angley@protonmail.com>
2023-02-28 05:29:14 -0500

    update genssl

--- scripts/genssl.sh
+++ scripts/genssl.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
 # Generate SSL certificate to sslcert/server.key and sslcert/server.cert
 
-SSLDIR="$(dirname 0)/../sslcert/"
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout server.key -out server.crt
 
-mkdir -p "$SSLDIR"
-
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $SSLDIR/server.key -out $SSLDIR/server.crt
+echo "Output certificate to server.key and server.crt"
+echo "Make sure to move these files to a directory called sslcert next to the package.json file"
