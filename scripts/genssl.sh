7828b94f1a93e4298fc7a1c3629841258452ada5
constellationz <t.angley@protonmail.com>
2023-03-02 02:52:56 -0500

    nuke repository for heroku migration

--- scripts/genssl.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-# Generate SSL certificate to sslcert/server.key and sslcert/server.cert
-
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout server.key -out server.crt
-
-sudo chmod 644 server.key server.crt
-
-echo "Output certificate to server.key and server.crt"
-echo "Make sure to move these files to a directory called sslcert next to the package.json file"
