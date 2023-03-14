51039a2c213e892183749eb2c3f5103ff58c12fc
Ryan Turner <RyanTurner02@protonmail.com>
2023-03-14 18:35:15 -0400

    Add bootstrap styling to search bar

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,5 +1,5 @@
 const Search = ({ taskName }) => {
-  return <input className="search-bar" placeholder="Search" />;
+  return <input className="form-control search-bar" placeholder="Search" />;
 };
 
 export default Search;
