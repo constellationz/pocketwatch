6260ceb9d6d0fe006ea2d36eb9a12022efc115de
Ryan Turner <RyanTurner02@protonmail.com>
2023-03-14 17:56:53 -0400

    Add timer component

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,19 @@
 // Dashboard.jsx
 // Main timer/dashboard page
 
+import Timer from "../components/Timer";
+
 function Dashboard() {
-  return <div>Dashboard</div>;
+  let currentTask = {
+    name: "Task 1",
+    timeElapsed: "00:00:00",
+  };
+
+  return (
+    <div>
+      <Timer time={currentTask.timeElapsed} />
+    </div>
+  );
 }
 
 export default Dashboard;
