d2d89242c64575affb3552dbd4b1d07221be09d8
Ryan Turner <RyanTurner02@protonmail.com>
2023-03-15 17:06:12 -0400

    Make Current Task component responsive

--- frontend/src/components/CurrentTask.js
+++ frontend/src/components/CurrentTask.js
@@ -2,10 +2,14 @@ import Button from "react-bootstrap/Button";
 
 const CurrentTask = ({ props }) => {
   return (
-    <div className="d-flex justify-content-center">
-      <input className="form-control" placeholder={props.name} />
-      <Button id="media-control">Play/Pause</Button>
-      <Button id="add-task">Add Task</Button>
+    <div className="row">
+      <div className="d-flex justify-content-start col-md-8 col-12">
+        <input className="form-control" placeholder={props.name} />
+      </div>
+      <div className="d-flex justify-content-around col-md-4 col-12">
+          <Button id="media-control">Play / Pause</Button>
+          <Button id="add-task">Add Task</Button>
+      </div>
     </div>
   );
 };
