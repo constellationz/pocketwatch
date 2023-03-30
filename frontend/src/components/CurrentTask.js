ee9128475365d883bf00861a3f8aaa1a7f971953
Ryan Turner <RyanTurner02@protonmail.com>
2023-03-30 13:16:57 -0400

    Increase current task name input size

--- frontend/src/components/CurrentTask.js
+++ frontend/src/components/CurrentTask.js
@@ -3,10 +3,10 @@ import Button from "react-bootstrap/Button";
 const CurrentTask = ({ props }) => {
   return (
     <div className="row mb-3">
-      <div className="d-flex justify-content-start col-md-8 col-12 current-task-name">
+      <div className="d-flex justify-content-start col-md-10 col-12 current-task-name">
         <input className="form-control" placeholder={props.name} />
       </div>
-      <div className="d-flex col-md-4 col-12 current-task-buttons">
+      <div className="d-flex col-md-2 col-12 current-task-buttons">
           <Button variant="dark" id="media-control">Play</Button>
           <Button variant="dark" id="add-task">+</Button>
       </div>
