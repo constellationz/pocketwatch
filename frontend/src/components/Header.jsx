a8c373664ba7b8948218568fba0763b8cd0c3171
Ryan Turner <RyanTurner02@protonmail.com>
2023-04-04 11:05:23 -0400

    Add responsive navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,40 +1,29 @@
 // Header.jsx
 // Header for the webpages
 
-import React from "react";
-import { FaSignInAlt, FaSignOutAlt, FaUserAlt } from "react-icons/fa";
-import { BsGearFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function Header() {
   return (
-    <header className="header">
-      <div className="logo">
-        <Link to="/">Pocketwatch</Link>
-      </div>
-      <ul>
-        <li>
-          <Link to="/login">
-            <FaSignInAlt /> Login
-          </Link>
-        </li>
-        <li>
-          <Link to="/register">
-            <FaUserAlt /> Register
-          </Link>
-        </li>
-        <li>
-          <Link to="/register">
-            <FaSignOutAlt /> Logout
-          </Link>
-        </li>
-        <li>
-          <Link to="/settings">
-            <BsGearFill /> Settings
-          </Link>
-        </li>
-      </ul>
-    </header>
+    <Navbar bg="light" expand="md">
+      <Container>
+        <Navbar.Brand>
+          <Link className="navbar-brand" to="/">Pocketwatch</Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav>
+            <Link className="nav-link" to="/login">Login</Link>
+            <Link className="nav-link" to="/register">Register</Link>
+            <Link className="nav-link" to="/settings">Settings</Link>
+            <Link className="nav-link" to="/logout">Logout</Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
