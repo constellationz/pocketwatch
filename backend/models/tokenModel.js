19cc077da254924f9447407b16686f9a16fbb89d
constellationz <t.angley@protonmail.com>
2023-04-11 13:15:27 -0400

    add tokenModel

---
+++ backend/models/tokenModel.js
@@ -0,0 +1,24 @@
+// tokenModel.js
+// Schema for the token resource
+
+const mongoose = require("mongoose");
+
+const tokenSchema = mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+    expires: 600,
+  },
+});
+
+module.exports = mongoose.model("Token", tokenSchema);
