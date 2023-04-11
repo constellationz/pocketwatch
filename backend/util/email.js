710934161e39b64542c5b5396ebb3cb4e613c465
constellationz <t.angley@protonmail.com>
2023-04-11 13:01:40 -0400

    Add email function and updated environment example file

---
+++ backend/util/email.js
@@ -0,0 +1,47 @@
+// email.js
+// Contains functionality for email
+
+const nodemailer = require("nodemailer");
+const handlebars = require("handlebars");
+const fs = require("fs");
+const path = require("path");
+
+const sendEmail = async (email, subject, payload, template) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      },
+    });
+
+    const source = fs.readFileSync(path.join(__dirname, template), "utf8");
+    const compiledTemplate = handlebars.compile(source);
+    const options = () => {
+      return {
+        from: process.env.FROM_EMAIL,
+        to: email,
+        subject: subject,
+        html: compiledTemplate(payload),
+      };
+    };
+
+    transporter.sendMail(options(), (error, info) => {
+      if (error) {
+        return error;
+      }
+
+      return res.status(200).json({
+        success: true,
+      })
+    });
+  } catch (error) {
+    return error;
+  }
+}
+
+module.exports = {
+  sendEmail
+}
