8b9a33f52b22612054e4c5539cdbc5bbeaae3513
koxborough <oxboroughkhalil@gmail.com>
2023-04-14 11:27:14 -0400

    Create tests for update email/password endpoints

--- spec/UserSimulation.spec.js
+++ spec/UserSimulation.spec.js
@@ -1,5 +1,7 @@
+const bcrypt = require("bcryptjs");
+
 describe("User Simulation: ", function() {
-    let user;
+    let user, token;
 
     describe("registering new user", function() {
         let apiResponse, status;
@@ -65,6 +67,79 @@ describe("User Simulation: ", function() {
 
         it("should return user data", function() {
             expect(Object.keys(user)).toContain("token");
+        });
+    
+        it("should give 200 status code", function() {
+            expect(status).toEqual(200);
+        });
+    });
+
+    describe("updating user email", function() {
+        let status;
+
+        beforeAll(async function() {
+            token = user.token;
+            await fetch("http://localhost:5000/api/users/updateEmail", {
+                method: "POST",
+                crossDomain: true,
+                headers: {
+                    "Authorization": `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                    "Access-Control-Allow-Origin": "*"
+                },
+                body: JSON.stringify({
+                    password: "password",
+                    newEmail: "updateEmail@gmail.com"
+                })
+            })
+            .then(res => {
+                status = res.status;
+                return res.json()
+            })
+            .then(data => {
+                user = data;
+            });
+        });
+
+        it("should return user data", function() {
+            expect(user.email).toEqual("updateEmail@gmail.com");
+        });
+    
+        it("should give 200 status code", function() {
+            expect(status).toEqual(200);
+        });
+    });
+
+    describe("updating user password", function() {
+        let status;
+
+        beforeAll(async function() {
+            await fetch("http://localhost:5000/api/users/updatePassword", {
+                method: "POST",
+                crossDomain: true,
+                headers: {
+                    "Authorization": `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                    "Access-Control-Allow-Origin": "*"
+                },
+                body: JSON.stringify({
+                    password: "password",
+                    newPassword: "newPassword"
+                })
+            })
+            .then(res => {
+                status = res.status;
+                return res.json()
+            })
+            .then(data => {
+                user = data;
+            });
+        });
+
+        it("should return user data", async function() {
+            expect(await bcrypt.compare("newPassword", user.password)).toEqual(true, "newPassword does not equal hashed password.");
         });
     
         it("should give 200 status code", function() {
