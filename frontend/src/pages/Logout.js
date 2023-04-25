29bce315c89d90f89815d64456691d4d9823bf80
Ryan Turner <RyanTurner02@protonmail.com>
2023-04-25 15:37:29 -0400

    Remove unused logout code

--- frontend/src/pages/Logout.js
+++ frontend/src/pages/Logout.js
@@ -1,14 +1,10 @@
-import { useEffect } from "react";
 
 const Logout = () => {
-
   // Log user out
   localStorage.removeItem("token");
 
   // Redirect to homepage
-  useEffect(() => {
-    window.location.replace("/");
-  });
+  window.location.replace("/");
 };
 
 export default Logout;
