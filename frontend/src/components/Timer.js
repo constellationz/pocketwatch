5521c8267e1454c762ef679b23cb611f03a9159c
Christian Lopez <christianlopez@christianlopezmac.local>
2023-05-01 15:32:47 -0400

    readded the conditionals in the timer page

--- frontend/src/components/Timer.js
+++ frontend/src/components/Timer.js
@@ -1,6 +1,15 @@
 import React from "react";
 
 const Timer = ({ timerHours, timerMinutes, timerSeconds }) => {
+  if(timerSeconds >= 60) {
+    timerSeconds %= 60;
+    timerMinutes++;
+  }
+
+  if(timerMinutes >= 60) {
+    timerMinutes %= 60;
+    timerHours++;
+  }
 
   const paddedHours = String(timerHours).padStart(2, '0');
   const paddedMinutes = String(timerMinutes).padStart(2, '0');
